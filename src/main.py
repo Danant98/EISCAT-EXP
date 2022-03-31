5016e2efdd301d3971d77ff0f310bbc10a2c12f3
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-03-31 18:29:18 +0200

    Updated main module

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ Assignment based on data obtained from experiment done at the EISCAT radar near 
 # Import libraries and modules
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-
+import os
+# Import dataset
+dataset = np.loadtxt(os.path.join("resources", "2022-03-24_beata_5@uhfa 1.tgz"))
+print(dataset)
